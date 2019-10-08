d854e962ebba0abb22dc8af0e9e9c1116401f243
aa <aa@aa.aa>
2019-10-08 16:26:22 -0400

    After starting Node server

---
+++ app.js
@@ -0,0 +1,15 @@
+var express= require("express");
+var app= express();
+var path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/assets', express.static('assets'));
+//app.use(express.static(path.join(__dirname, 'assets/stylesheet')));
+
+app.get("/", function(req, res){
+    res.render('index');
+})
+
+app.listen(8080);
